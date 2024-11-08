f90aaf61c2c4c17b1d56bc67cef6e2c1ef8c677e
intezya <intezya@gmail.com>
2024-11-08 22:01:05 +0300

    Update models

--- app/internal/db/models.py
+++ app/internal/db/models.py
@@ -1,37 +1,38 @@
 import uuid
 from typing import List
 
+from pydantic import field_validator
 from sqlmodel import Field, SQLModel
 
 
 
 class Debtor(SQLModel):
     name: str
-    debt: float
+    value: float
 
 
 class Event(SQLModel, table=True):
-    bill_id: uuid.UUID = Field(primary_key=True, nullable=False)
+    id: uuid.UUID = Field(primary_key=True, nullable=False)
     owner_name: str
-
     event_name: str
-
     debts: List[Debtor, ...] = Field(default_factory=list)
 
 
 class Trip(SQLModel, table=True):
-    bill_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     trip_name: str
     event_ids: List[uuid.UUID] = Field(default_factory=list)
 
 
-class Bill(SQLModel, table=True):
-    bill_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    owner_name: str
-    bill_type: str = "event"
-    users: List[uuid.UUID] = Field(default_factory=list)
-
 
 class Link(SQLModel, table=True):
     value: str = Field(primary_key=True)
-    bill_id: uuid.UUID
+    id: uuid.UUID
+    type: str
+
+    @classmethod
+    @field_validator("type")
+    def validate_link_type(cls, v):
+        if v not in ["event", "trip"]:
+            raise ValueError("type must be 'event' or 'trip'")
+        return v
