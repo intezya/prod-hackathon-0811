7f7fb5938ec83eab77e6e785169ac498e57021cf
intezya <intezya@gmail.com>
2024-11-08 20:03:47 +0300

    Add models

--- app/internal/db/models.py
+++ app/internal/db/models.py
@@ -1,19 +1,44 @@
 import uuid
+from typing import List
 
-from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 
-class UserBase(SQLModel):
-    email: EmailStr = Field(unique=True, index=True, max_length=255)
-    username: str = Field(unique=True, index=True, min_length=5, max_length=32)
-    name: str | None = Field(default=None, max_length=255)
-
-
-
-class User(UserBase, table=True):
+class EventOwner(SQLModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    hashed_password: str
+    name: str
 
 
-    # Shared properties
+class Debtor(SQLModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    name: str
+    debt: float
+
+
+class BaseEvent(SQLModel):
+    bill_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    owner_id: uuid.UUID = Field(default_factory=uuid.uuid4, index=True)
+
+    event_name: str
+
+    debts: List[EventOwner, Debtor, ...] = Field(default_factory=list)
+
+
+class Trip(SQLModel):
+    bill_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    trip_name: str
+    event_ids: List[uuid.UUID] = Field(default_factory=list)
+
+
+
+
+class Bills(SQLModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    event_id: ... # TODO
+
+    owner_id: uuid.UUID = Field(default_factory=uuid.uuid4, index=True)
+
+    name: str
+    bill_type: str = "event"
+
+    trips: List[Trip, ...] = Field(default_factory=list)
