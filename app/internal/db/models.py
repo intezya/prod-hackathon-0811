524da20a4067276089c164198a002d3ea7fc322b
intezya <intezya@gmail.com>
2024-11-08 21:16:55 +0300

    Add link model

--- app/internal/db/models.py
+++ app/internal/db/models.py
@@ -10,7 +10,7 @@ class Debtor(SQLModel):
     debt: float
 
 
-class BaseEvent(SQLModel):
+class Event(SQLModel, table=True):
     bill_id: uuid.UUID = Field(primary_key=True, nullable=False)
     owner_name: str
 
@@ -19,14 +19,19 @@ class BaseEvent(SQLModel):
     debts: List[Debtor, ...] = Field(default_factory=list)
 
 
-class Trip(SQLModel):
+class Trip(SQLModel, table=True):
     bill_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     trip_name: str
     event_ids: List[uuid.UUID] = Field(default_factory=list)
 
 
-class Bills(SQLModel):
+class Bill(SQLModel, table=True):
     bill_id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     owner_name: str
     bill_type: str = "event"
     users: List[uuid.UUID] = Field(default_factory=list)
+
+
+class Link(SQLModel, table=True):
+    value: str = Field(primary_key=True)
+    bill_id: uuid.UUID
