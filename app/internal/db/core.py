47c1a42b9fa39758bba2fc0c2fe09720fcc4985e
intezya <intezya@gmail.com>
2024-11-09 16:36:16 +0300

    Update db core

--- app/internal/db/core.py
+++ app/internal/db/core.py
@@ -1,14 +1,23 @@
-from typing import AsyncGenerator
-
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.internal.config import settings
 
 
-engine = create_async_engine(str(settings.POSTGRES_DATABASE_URI))
+def __get_engine(url: str = str(settings.POSTGRES_DATABASE_URI)) -> AsyncEngine:
+    return create_async_engine(url)
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSession(engine) as session:
+
+__local_session: async_sessionmaker = async_sessionmaker(
+    bind=__get_engine(),
+    class_=AsyncSession,
+    expire_on_commit=False,  # Don't expire objects after commit
+    autoflush=False,  # Don't auto-flush changes to DB
+    autocommit=False,  # Keep everything in transactions
+)
+
+
+async def get_db() -> AsyncSession:
+    async with __local_session() as session:
         yield session
         await session.close()
