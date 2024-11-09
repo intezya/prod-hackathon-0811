d5cf341993d33bcf46cc11fa3cd84c3d23be6889
glorp <glorp@spaceship.com>
2024-11-09 18:36:44 +0300

    update debtors

--- app/api/requests/debt.py
+++ app/api/requests/debt.py
@@ -2,13 +2,11 @@ from sqlmodel import SQLModel
 
 
 class PayDebtRequest(SQLModel):
-    trip_id: str
     event_id: str
     debtor_name: str
     pay_value: float
 
 
 class ForgiveDebtRequest(SQLModel):
-    trip_id: str
     event_id: str
     debtor_name: str
