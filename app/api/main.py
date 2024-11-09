80856c81b50ec31a896aaf65fe823f8518607b26
intezya <intezya@gmail.com>
2024-11-09 17:35:39 +0300

    Fix typo

--- app/api/main.py
+++ app/api/main.py
@@ -8,7 +8,7 @@ api_router.include_router(debt.router, prefix="/debt", tags=["debts"])
 api_router.include_router(event.router, prefix="/event", tags=["events"])
 api_router.include_router(trip.router, prefix="/trip", tags=["trip"])
 api_router.include_router(websocket.router, prefix="/websocket", tags=["websocket"])
-api_router.include_router(link.router, prefix="/link", tags=["websocket"])
+api_router.include_router(link.router, prefix="/link", tags=["link"])
 
 
 @api_router.get("/health-check")
