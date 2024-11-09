f06bf01085f8b6dee95ca8ea85bb92ec2918c7ed
intezya <intezya@gmail.com>
2024-11-09 17:38:17 +0300

    trip request field validation

--- app/api/requests/trip.py
+++ app/api/requests/trip.py
@@ -1,9 +1,18 @@
+from fastapi import HTTPException
+from pydantic import field_validator
 from sqlmodel import SQLModel
+from starlette import status
 
 
 class CreateTripRequest(SQLModel):
     name: str
     allowed_names: str
+
+    @classmethod
+    @field_validator("allowed_names")
+    def validate_debts(cls, value):
+        if len(value) != len(set(value)):
+            raise HTTPException(status.HTTP_400_BAD_REQUEST)
 
 
 class DeleteTripRequest(SQLModel):
