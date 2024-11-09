b5cf5ff9a9587fbe1cb2c3009438d6eb32b6ca38
intezya <intezya@gmail.com>
2024-11-09 11:07:18 +0300

    Use async sqlmodel

--- app/internal/db/core.py
+++ app/internal/db/core.py
@@ -1,8 +1,14 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+
 from app.internal.config import settings
-from sqlmodel import Session, create_engine
 
-engine = create_engine(str(settings.POSTGRES_DATABASE_URI))
 
-def get_db():
-    with Session(engine) as session:
+engine = create_async_engine(str(settings.POSTGRES_DATABASE_URI))
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSession(engine) as session:
         yield session
+        yield session.close()
